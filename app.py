8f51554df2853a9e30aefe142ec67f5bf95c6f2b
Moiz Sajid <moizsajid619@gmail.com>
2025-03-23 21:52:31 +0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,67 @@
+import streamlit as st
+import torch
+from torch import nn
+from torchvision.transforms import functional as F
+
+
+# Spatial size of training images. All images will be resized to this
+#   size using a transformer.
+image_size = 64
+
+# Number of channels in the training images. For color images this is 3
+nc = 3
+
+# Size of z latent vector (i.e. size of generator input)
+nz = 100
+
+# Size of feature maps in generator
+ngf = 64
+
+# Size of feature maps in discriminator
+ndf = 64
+
+class Generator(nn.Module):
+    def __init__(self, ngpu):
+        super(Generator, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            # input is Z, going into a convolution
+            nn.ConvTranspose2d( nz, ngf * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(ngf * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. ``(ngf*8) x 4 x 4``
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. ``(ngf*4) x 8 x 8``
+            nn.ConvTranspose2d( ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. ``(ngf*2) x 16 x 16``
+            nn.ConvTranspose2d( ngf * 2, ngf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. ``(ngf) x 32 x 32``
+            nn.ConvTranspose2d( ngf, nc, 4, 2, 1, bias=False),
+            nn.Tanh()
+            # state size. ``(nc) x 64 x 64``
+        )
+
+    def forward(self, input):
+        return self.main(input)
+
+model = torch.load("generator_model.pth", weights_only=False, map_location=torch.device('cpu'))
+model.eval()
+
+fixed_noise = torch.randn(1, nz, 1, 1, device="cpu")
+
+fake = model(fixed_noise).detach().cpu()[0]#.permute(1, 2, 0)
+
+st.title("This person and anime does not exist!")
+placeholder = st.image(F.to_pil_image(fake), caption="This person and anime does not exist!", use_container_width=True)
+
+if st.button("Press me!"):
+    placeholder.empty()
+    fixed_noise = torch.randn(1, nz, 1, 1, device="cpu")
+    fake = model(fixed_noise).detach().cpu()[0]  # .permute(1, 2, 0)
+    st.image(F.to_pil_image(fake), caption="This person and anime does not exist!", use_container_width=True)
